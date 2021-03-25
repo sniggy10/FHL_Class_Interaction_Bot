ff10586e4dff75db936c468e87dd400de7ee7225
sniggy <snigdhaansu@gmail.com>
2021-03-25 17:53:36 +0530

    Initial commit

---
+++ ContentBubble/Controllers/TabController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Content_Bubble_Bot
+{
+    public class TabController : Controller
+    {
+
+        [Route("first")]
+        public ActionResult First()
+        {
+            StringBuilder html = new StringBuilder();
+            html.Append("<div> Hello world </div>");
+            ViewBag.Message = html;
+            return View();
+        }
+    }
+}
